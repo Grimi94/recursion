e2ca9554386e5a01cc87948a81baf7ca9b47a5c9
Antonio Grimaldo <grimi94@gmail.com>
2014-05-20 18:32:44 -0500

    Base cases are working (values, strings and empty objects)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,20 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringified = '';
+  if( obj instanceof String || typeof obj === 'string'){
+  	return "\""+obj+"\"";
+  }
+  else if (typeof obj === 'number' || typeof obj === 'boolean'){
+  	return ""+obj+"";
+  }
+  else if (Array.isArray(obj)) {
+  	if (!obj.length) return "[]";
+
+  }
+  else if (typeof obj === 'object'){
+  	return ""+obj+"";
+
+  }
+
 };
