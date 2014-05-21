751eea03171f0e0a735d4b0807114837f70bbfdd
Antonio Grimaldo <grimi94@gmail.com>
2014-05-20 21:08:47 -0500

    Strinigify now pases all stringifiable objects, missing unstringifiable values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,8 +4,9 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
-  
-  if( obj instanceof String || typeof obj === 'string'){
+  if (obj === null) { return 'null';}
+  else if (typeof obj === 'function' || typeof obj === 'undefined') {return '';} 
+  else if( obj instanceof String || typeof obj === 'string'){
   	return "\""+obj+"\"";
   }
   else if (typeof obj === 'number' || typeof obj === 'boolean'){
@@ -19,7 +20,11 @@ var stringifyJSON = function(obj) {
   	return "["+stringified+"]";
   }
   else if (typeof obj === 'object'){
-  	return ""+obj+"";
+  	var stringified = [];
+  	_.each(obj,function(val, key){
+  		stringified.push("\""+key+"\""+':'+stringifyJSON(val));
+  	});
+  	return "{"+stringified+"}";
 
   }
 
