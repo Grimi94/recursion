5d8da5f226aebd6254ead7d1d1028a43fc041662
Antonio Grimaldo <grimi94@gmail.com>
2014-05-22 23:44:39 -0500

    First solution to getElementsByClassName, but it is slow

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,36 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	var nodes;
+	var regExp = new RegExp("(?:^|\\s)"+className+"(?:\\s|$)");
+	var newArr = [];
+	if (this.hasOwnProperty('document')) {
+		if (document.body.className.match(className)) {
+			newArr.push(document.body);
+		}
+		nodes = this.document.body.children;
+	}
+	else{
+		nodes = this.children;
+	}
+	
+	
+	_.each(nodes, function(value){
+		//var length = value.children.length;
+		//if (length !== 0 && length !== undefined) console.log(true);
+		if (regExp.test(value.className)) {
+			var temp =  value;
+			newArr.push(temp);
+		}
+		else if (value.hasOwnProperty('children')) {
+			if (value.children.length > 0){
+				var temp = _.flatten(getElementsByClassName.call(value,className));
+				if (temp.length) {newArr.push(temp[0]);}
+				
+			}
+		}
+		
+	});
+	console.log(newArr);
+	return newArr;
 };
