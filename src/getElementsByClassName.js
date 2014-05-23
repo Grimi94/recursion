6f6772757ca4f52dc9782029ed99473b72b42538
Antonio Grimaldo <grimi94@gmail.com>
2014-05-22 23:50:10 -0500

    getElementsByClassName finished, console.log was making it slow

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,34 +7,31 @@
 var getElementsByClassName = function(className){
 	var nodes;
 	var regExp = new RegExp("(?:^|\\s)"+className+"(?:\\s|$)");
-	var newArr = [];
+	var result = [];
 	if (this.hasOwnProperty('document')) {
 		if (document.body.className.match(className)) {
-			newArr.push(document.body);
+			result.push(document.body);
 		}
 		nodes = this.document.body.children;
 	}
 	else{
 		nodes = this.children;
 	}
-	
-	
 	_.each(nodes, function(value){
-		//var length = value.children.length;
-		//if (length !== 0 && length !== undefined) console.log(true);
 		if (regExp.test(value.className)) {
 			var temp =  value;
-			newArr.push(temp);
+			result.push(temp);
 		}
 		else if (value.hasOwnProperty('children')) {
 			if (value.children.length > 0){
-				var temp = _.flatten(getElementsByClassName.call(value,className));
-				if (temp.length) {newArr.push(temp[0]);}
+				var temp = _.flatten( getElementsByClassName.call(value, className) );
 				
+				if (temp.length) {
+					result.push(temp[0]);
+				}
 			}
 		}
-		
 	});
-	console.log(newArr);
-	return newArr;
+
+	return result;
 };
